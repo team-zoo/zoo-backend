3662fd6a3fc55b731e4c7c0139c0408d6f4248f8
Aaron <AMDennis1987@gmail.com>
2019-02-01 17:41:42 -0800

    Zoo model and test (#3)
    * pulling in utils for data helper function
    
    * Zoo models with tests

---
+++ __tests__/models/zoo.test.js
@@ -0,0 +1,20 @@
+require('../../dataHelper');
+const Zoo = require('../../lib/models/Zoo');
+
+describe('zoo model', () => {
+  it('validates a good zoo model', () => {
+    const zoo = {
+      photoUrl: 'Great Zoo',
+      name: 'Brooklyn Zoo',
+      city: 'Brooklyn'
+    };
+    const newZoo = new Zoo(zoo);
+    const jsonZoo = newZoo.toJSON();
+    expect(jsonZoo).toEqual({
+      photoUrl: 'Great Zoo',
+      name: 'Brooklyn Zoo',
+      city: 'Brooklyn',
+      _id: expect.any(Object)
+    });
+  });
+});
