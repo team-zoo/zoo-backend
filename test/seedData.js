9bf1500552e2568649187cc11f70c47c5f2b2490
Kristin Hortsch <42101837+kristinhortsch@users.noreply.github.com>
2019-02-04 13:54:27 -0800

    updated visitor model to tie to user model (#17)
    * added auth.js
    
    * added ensure auth array to /verify route in auth.js
    
    * updated visitor model to tie to user model

--- test/seedData.js
+++ test/seedData.js
@@ -1,17 +1,11 @@
 const Chance = require('chance');
 const chance = new Chance();
 const zooData = require('./data/zooData');
+const animalData = require('./data/animalData');
 const Zoo = require('../lib/models/Zoo');
 const Visitor = require('../lib/models/Visitor');
 const Animal = require('../lib/models/Animal');
-const animalData = require('./data/animalData');
-
-console.log(animalData);
-
-// const DEFAULT_TOTAL_USERS = 10;
-// const DEFAULT_TOTAL_ZOOS = 5;
-// const DEFAULT_TOTAL_VISITORS = 20;
-// const DEFAULT_TOTAL_ANIMALS = 30;
+// const User = require('../lib/models/User');
 
 const seedData = () => {
   return Promise.all(
@@ -29,7 +23,7 @@ const seedData = () => {
           return Promise.all(
             [...Array(100)].map(() => {
               return Visitor.create({
-                username: chance.email(),
+                username: chance.pickone(users),
                 zoo: chance.pickone(zoos)._id,
                 age: chance.age(),
                 favoriteAnimal: [chance.pickone(animals)._id, chance.pickone(animals)._id, chance.pickone(animals)._id, chance.pickone(animals)._id, chance.pickone(animals)._id]
