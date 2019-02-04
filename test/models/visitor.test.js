dcb7a7a92bdda11d221288a6374ff8d700f181c6
Marcy Silverman <marcysilve225@gmail.com>
2019-02-04 10:14:51 -0800

    grabs all visitors and by id passes

--- test/models/visitor.test.js
+++ test/models/visitor.test.js
@@ -28,10 +28,44 @@ describe('visitor app', () => {
     mongoose.connection.close(done);
   });
   it('tests the visitor model', () => {
-    const visitor = new Visitor({ username: 'Bob1' });
-    expect(visitor.toJSON()).toEqual({ username: 'Bob1', _id: expect.any(Types.ObjectId) });
+    const visitor = new Visitor({ username: 'Leslie' });
+    expect(visitor.toJSON()).toEqual({ username: 'Leslie', _id: expect.any(Types.ObjectId) });
   });
-  it('can create a visitor', () => {
-    
-  })
+  it('can create a visitor', () => {  
+    return request(app)
+      .post('/visitors')
+      .send({ username: 'Jerry', age: 12 })
+      .then(res => {
+        expect(res.body).toEqual({
+          username: 'Jerry',
+          _id: expect.any(String),
+          age: 12
+        });
+      });
+  });
+  it('get all visitors', () => {
+    return Promise.all(['Ron', 'April', 'Anne'].map(createVisitor))
+      .then(() => {
+        return request(app)
+          .get('/visitors');
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(3);
+      });
+  });
+  it('can get a visitor by Id', () => {
+    return createVisitor('Tom')
+      .then(visitor => {
+        return request(app)
+          .get(`/visitors/${visitor._id}`)
+          .then(res => {
+            expect(res.body).toEqual({
+              username: 'Tom',
+              _id: expect.any(String),
+              age: 12
+            });
+          });
+      });
+
+  });
 });
