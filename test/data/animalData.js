ead0518a69b5b1e1a62627b807ad6dd60d57fddb
Kristin Hortsch <kristinhortsch@gmail.com>
2019-02-04 09:56:39 -0800

    created animal data js

---
+++ test/data/animalData.js
@@ -0,0 +1,219 @@
+module.exports = [
+  {
+    name: 'cheetah',
+    type: 'mammal'
+  },
+  {
+    name: 'brown bear',
+    type: 'mammal'
+  },
+  {
+    name: 'beluga whale',
+    type: 'mammal'
+  },
+  {
+    name: 'dolphin',
+    type: 'mammal'
+  },
+  {
+    name: 'giraffe',
+    type: 'mammal'
+  },
+  {
+    name: 'lion',
+    type: 'mammal'
+  },
+  {
+    name: 'otter',
+    type: 'mammal'
+  },
+  {
+    name: 'siberian tiger',
+    type: 'mammal'
+  },
+  {
+    name: 'wolf',
+    type: 'mammal'
+  },
+  {
+    name: 'polar bear',
+    type: 'mammal'
+  },
+  {
+    name: 'gazelle',
+    type: 'mammal'
+  },
+  {
+    name: 'hedgehog',
+    type: 'mammal'
+  },
+  {
+    name: 'beaver',
+    type: 'mammal'
+  },
+  {
+    name: 'python',
+    type: 'reptile'
+  },
+  {
+    name: 'tortoise',
+    type: 'reptile'
+  },
+  {
+    name: 'crocodile',
+    type: 'reptile'
+  },
+  {
+    name: 'gila monster',
+    type: 'reptile'
+  },
+  {
+    name: 'iguana',
+    type: 'reptile'
+  },
+  {
+    name: 'chameleon',
+    type: 'reptile'
+  },
+  {
+    name: 'painted turtle',
+    type: 'reptile'
+  },
+  {
+    name: 'king cobra',
+    type: 'reptile'
+  },
+  {
+    name: 'gecko',
+    type: 'reptile'
+  },
+
+  {
+    name: 'viper',
+    type: 'reptile'
+  },
+  {
+    name: 'macaw',
+    type: 'bird'
+  },
+  {
+    name: 'great blue heron',
+    type: 'bird'
+  },
+  {
+    name: 'flamingo',
+    type: 'bird'
+  },
+  {
+    name: 'egret',
+    type: 'bird'
+  },
+  {
+    name: 'puffin',
+    type: 'bird'
+  },
+  {
+    name: 'ostrich',
+    type: 'bird'
+  },
+  {
+    name: 'peacock',
+    type: 'bird'
+  },
+  {
+    name: 'great horned owl',
+    type: 'bird'
+  },
+  {
+    name: 'king penguin',
+    type: 'bird'
+  },
+  {
+    name: 'bald eagle',
+    type: 'bird'
+  },
+  {
+    name: 'american bullfrog',
+    type: 'amphibian'
+  },
+  {
+    name: 'american toad',
+    type: 'amphibian'
+  },
+  {
+    name: 'newt',
+    type: 'amphibian'
+  },
+  {
+    name: 'salamander',
+    type: 'amphibian'
+  },
+  {
+    name: 'tree frog',
+    type: 'amphibian'
+  },
+  {
+    name: 'leopard frog',
+    type: 'amphibian'
+  },
+  {
+    name: 'bullfrog',
+    type: 'amphibian'
+  },
+  {
+    name: 'tomato frog',
+    type: 'amphibian'
+  },
+  {
+    name: 'giant marine toad',
+    type: 'amphibian'
+  },
+  {
+    name: 'moss frog',
+    type: 'amphibian'
+  },
+  {
+    name: 'sturgeon',
+    type: 'fish'
+  },
+  {
+    name: 'rainbow trout',
+    type: 'fish'
+  },
+  {
+    name: 'coho salmon',
+    type: 'fish'
+  },
+  {
+    name: 'chinook salmon',
+    type: 'fish'
+  },
+  {
+    name: 'bull trout',
+    type: 'fish'
+  },
+  {
+    name: 'african lungfish',
+    type: 'fish'
+  },
+  {
+    name: 'pirahna',
+    type: 'fish'
+  },
+  {
+    name: 'swordfish',
+    type: 'fish'
+  },
+  {
+    name: 'bluehead wrasse',
+    type: 'fish'
+  },
+  {
+    name: 'seahorse',
+    type: 'fish'
+  },
+  {
+    name: 'stringray',
+    type: 'fish'
+  }
+];
--- test/seedData.js
+++ test/seedData.js
@@ -1,15 +1,30 @@
-// const Chance = require('chance');
-// const chance = new Chance();
+const Chance = require('chance');
+const chance = new Chance();
 const zooData = require('./data/zooData');
 const Zoo = require('../lib/models/Zoo');
-
+const Visitor = require('../lib/models/Visitor');
+const Animal = require('../lib/models/Animal');
+const animalData = require('./data/animalData');
 
 const seedData = () => {
   return Promise.all(
     zooData.map(zoo => {
       return Zoo.create(zoo);
     })
-  );
+  )
+    .then(zoos => {
+      return Promise.all(
+        [...Array(15)].map(() => {
+          return Visitor.create({
+            username: chance.email(),
+            zoo: chance.pickone(zoos)._id,
+            age: 25,
+            animals: chance.pickone(animals)._id
+          });
+        })
+      );
+    });
 };
 
 module.exports = seedData;
+
