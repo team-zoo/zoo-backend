ef7c88a3df44d57cddb69c4e495b388cb5b2c0be
Marcy Silverman <marcysilve225@gmail.com>
2019-02-04 14:28:24 -0800

    started auth test

---
+++ test/routes/auth.test.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+require('../../lib/utils/connect')();
+const connect = require('../../lib/utils/connect');
+const app = require('../../lib/app');
+const User = require('../../lib/models/User');
+const request = require('supertest');
+const mongoose = require('mongoose');
+
+describe('User Model', () => {
+  const createUser = (username) => {
+    return User.create({
+      username, 
+      password: 'password'
+    })
+      .then(res =>  res.body);
+  };
+  beforeAll(() => {
+    connect();
+  });
+  beforeEach(done => {
+    mongoose.connection.dropDatabase(done);
+  });
+  afterAll(done => {
+    mongoose.connection.close(done);
+  });
+});
