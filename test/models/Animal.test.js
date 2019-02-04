7d451ab0fc500a75ccb45adb7680021d1188f753
Marcy Silverman <marcysilve225@gmail.com>
2019-02-04 14:08:20 -0800

    animal model passes

--- test/models/Animal.test.js
+++ test/models/Animal.test.js
@@ -1,25 +1,22 @@
-require('../dataHelper');
 const Animal = require('../../lib/models/Animal');
-const { getZoo } = require('../dataHelper');
+const Zoo = require('../../lib/models/Zoo');
+const mongoose = require('mongoose');
 
 describe('animal model', () => {
   it('validates a good animal model', () => {
-    return getZoo()
-      .then(createdZoo => {
-        const animal = {
-          zoo: createdZoo._id,
-          name: 'tiger',
-          type: 'mammal'
-        };
-        const newAnimal = new Animal(animal);
-        const jsonAnimal = newAnimal.toJSON();
-        
-        expect(jsonAnimal).toEqual({
-          zoo: expect.any(Object),
-          name: 'tiger',
-          type: 'mammal',
-          _id: expect.any(Object)
-        });
-      });
+    const zoo = new Zoo({ photoUrl: 'photo', name: 'San Diego zoo', city: 'San Diego' });
+    const animal = new Animal({ 
+      zoo: zoo._id, 
+      name: 'penguin', 
+      type: 'bird', 
+      status: 'alive' 
+    });
+    expect(animal.toJSON()).toEqual({ 
+      zoo: zoo._id, 
+      name: 'penguin', 
+      type: 'bird', 
+      status: 'alive',
+      _id: expect.any(mongoose.Types.ObjectId)
+    });
   });
 });
