be6c49f6e160790175db57dfe5551a3c859fe28f
Marcy Silverman <marcysilve225@gmail.com>
2019-02-04 14:01:24 -0800

    Visitor Model passes

--- test/models/Visitor.test.js
+++ test/models/Visitor.test.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const Visitor = require('../../lib/models/Visitor');
+const User = require('../../lib/models/User');
+const Zoo = require('../../lib/models/Zoo');
 
 describe('User model', () => {
   it('tests the visitor model', () => {
-    const visitor = new Visitor({ username: 'Leslie' });
-    expect(visitor.toJSON()).toEqual({ username: 'Leslie', _id: expect.any(mongoose.Types.ObjectId) });
+    const user = new User({ username: 'TOM', password: 'password' });
+    const zoo = new Zoo({ photoUrl: 'photo', name: 'San Diego zoo', city: 'San Diego' });
+    const visitor = new Visitor({ username: user._id, zoo: zoo._id, age: 15 });
+    expect(visitor.toJSON()).toEqual({ username: user._id, zoo: zoo._id, age: 15, _id: expect.any(mongoose.Types.ObjectId), favoriteAnimal: [] });
   });
 });
 
