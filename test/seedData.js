a6bb1c3541810ebdb63cc4db1f582842139d7024
Kristin Hortsch <42101837+kristinhortsch@users.noreply.github.com>
2019-02-04 14:28:21 -0800

    seed data is a GOOOOOOO (#20)
    * added auth.js
    
    * added ensure auth array to /verify route in auth.js
    
    * updated visitor model to tie to user model
    
    * cleaned up visitor model more
    
    * seed data with users is A GOOOOOOOO

--- test/seedData.js
+++ test/seedData.js
@@ -1,38 +1,51 @@
-// const Chance = require('chance');
-// const chance = new Chance();
-// const zooData = require('./data/zooData');
-// const animalData = require('./data/animalData');
-// const Zoo = require('../lib/models/Zoo');
-// const Visitor = require('../lib/models/Visitor');
-// const Animal = require('../lib/models/Animal');
-// // const User = require('../lib/models/User');
+const Chance = require('chance');
+const chance = new Chance();
+const zooData = require('./data/zooData');
+const animalData = require('./data/animalData');
+const Zoo = require('../lib/models/Zoo');
+const Visitor = require('../lib/models/Visitor');
+const Animal = require('../lib/models/Animal');
+const User = require('../lib/models/User');
 
-// const seedData = () => {
-//   return Promise.all(
-//     zooData.map(zoo => {
-//       return Zoo.create(zoo);
-//     })
-//   )
-//     .then(zoos => {
-//       return Promise.all(
-//         animalData(zoos).map(animal => {
-//           return Animal.create(animal);
-//         })
-//       )
-//         .then(animals => {
-//           return Promise.all(
-//             [...Array(100)].map(() => {
-//               return Visitor.create({
-//                 username: chance.pickone(users),
-//                 zoo: chance.pickone(zoos)._id,
-//                 age: chance.age(),
-//                 favoriteAnimal: [chance.pickone(animals)._id, chance.pickone(animals)._id, chance.pickone(animals)._id, chance.pickone(animals)._id, chance.pickone(animals)._id]
-//               });
-//             })
-//           );
-//         });
-//     });
-// };
+const seedData = () => {
+  return Promise.all(
+    [...Array(100)].map(() => {
+      return User.create({
+        username: chance.email(),
+        role: chance.pickone(['owner', 'visitor']),
+        password: 'password'
+      });
+    })
+  )
+    .then(users => {
+      return Promise.all(
+        zooData.map(zoo => {
+          return Zoo.create(zoo);
+        })
+      )
+        .then(zoos => {
+          return Promise.all(
+            animalData(zoos).map(animal => {
+              return Animal.create(animal);
+            })
+          )
+            .then(animals => {
+              return Promise.all(
+                [...Array(100)].map(() => {
+                  return Visitor.create({
+                    username: chance.pickone(users)._id,
+                    zoo: chance.pickone(zoos)._id,
+                    age: chance.age(),
+                    favoriteAnimal: [chance.pickone(animals)._id, chance.pickone(animals)._id, chance.pickone(animals)._id, chance.pickone(animals)._id, chance.pickone(animals)._id]
+                  });
+                })
+              );
+            });
+        });
 
-// module.exports = seedData;
+    });
+};
+
+
+module.exports = seedData;
 
