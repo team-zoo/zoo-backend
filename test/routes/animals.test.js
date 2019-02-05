ec192a4908ce50de6fa87752481a29fad2aace28
Shaba9 <shaba92@gmail.com>
2019-02-04 16:53:39 -0800

    get animal by id passes

--- test/routes/animals.test.js
+++ test/routes/animals.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const app = require('../../lib/app');
-const { getToken } = require('../dataHelper');
+const { getToken, getAnimal } = require('../dataHelper');
 
 describe('zoo model', () => {
   it('get a list of all animals', () => {
@@ -10,5 +10,20 @@ describe('zoo model', () => {
       .then(res => {
         expect(res.body).toHaveLength(54);
       });
+  });
+
+  it('get animal by id', () => {
+    return getAnimal()
+      .then(animal => {
+        return request(app)
+          .get(`/animals/${animal._id}`);
+      })
+      .then(res => expect(res.body).toEqual({
+        zoo: expect.any(String),
+        name: expect.any(String),
+        type: expect.any(String),
+        status: expect.any(String),
+        _id: expect.any(String)
+      }));
   });
 });
