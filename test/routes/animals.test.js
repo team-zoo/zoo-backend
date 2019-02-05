ccddf12ee01d413ebfca6d2ec4437f0a7d2e7415
Marcy Silverman <marcysilve225@gmail.com>
2019-02-04 17:22:35 -0800

    Animal route (#29)
    * post route works
    
    * post method is working
    
    * all animals pass

--- test/routes/animals.test.js
+++ test/routes/animals.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const app = require('../../lib/app');
 const { getToken, getAnimal } = require('../dataHelper');
+const Zoo = require('../../lib/models/Zoo');
 
 describe('zoo model', () => {
   it('get a list of all animals', () => {
@@ -26,4 +27,43 @@ describe('zoo model', () => {
         _id: expect.any(String)
       }));
   });
+  it('posts an animal', () => {
+    const zoo = new Zoo({ photoUrl: 'photo', name: 'San Diego zoo', city: 'San Diego' });
+    return request(app)
+      .post('/animals')
+      .set('Authorization', `Bearer ${getToken()}`)
+      .send({
+        zoo: zoo._id,
+        name: 'penguin',
+        type: 'bird',
+        status: 'alive'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          zoo: expect.any(String),
+          name: 'penguin',
+          type: 'bird',
+          status: 'alive',
+          _id: expect.any(String),  
+        });
+      });
+  });
+  it('can get an animal by id', () => {
+    return getAnimal()
+      .then(animal => {
+        return request(app)
+          .patch(`/animals/${animal._id}`)
+          .set('Authorization', `Bearer ${getToken()}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({ 
+          zoo: expect.any(String),
+          name: expect.any(String),
+          type: expect.any(String),
+          status: expect.any(String),
+          _id: expect.any(String)  
+        });
+      });
+  });
 });
+
