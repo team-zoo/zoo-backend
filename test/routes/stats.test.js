8e590c9b92febbb75d54b8b64436fc3201ec8cf4
Shaba9 <40212946+Shaba9@users.noreply.github.com>
2019-02-05 13:07:30 -0800

    Stats routes (#40)
    * get average age of each visitor passes
    
    * gets average age of visitors for all zoos passes
    
    * gets top 5 zoos by visitor count passes

---
+++ test/routes/stats.test.js
@@ -0,0 +1,38 @@
+require('../dataHelper');
+const request = require('supertest');
+const app = require('../../lib/app');
+
+describe('zoo model', () => {
+  it('gets average visitor age for all zoos', () => {
+    return request(app)
+      .get('/visitors/stats/average-age-all')
+      .then(res => expect(res.body[0]).toEqual({
+        _id: null,
+        avgAge: expect.any(Number)
+      }));
+  });
+
+  it('gets average visitor age for each zoo', () => {
+    return request(app)
+      .get('/visitors/stats/average-age-each')
+      .then(res => {
+        expect(res.body).toHaveLength(5);
+        res.body.forEach(avg => expect(avg).toEqual({
+          _id: expect.any(String),
+          avgAge: expect.any(Number)
+        }));
+      });
+  });
+
+  it('gets top 5 zoos by visitor count', () => {
+    return request(app)
+      .get('/visitors/stats/zoos-by-visitor-count')
+      .then(res => {
+        expect(res.body).toHaveLength(5);
+        res.body.forEach(count => expect(count).toEqual({
+          _id: expect.any(String),
+          visitorsCount: expect.any(Number)
+        }));
+      });
+  });
+});
