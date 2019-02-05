b4f3eb421b2d1c51aedb724ce973cd02fdff6c9a
Kristin Hortsch <kristinhortsch@gmail.com>
2019-02-04 16:27:21 -0800

    copied in auth test from dev

--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -1,26 +1,92 @@
-// require('dotenv').config();
-// require('../../lib/utils/connect')();
-// const connect = require('../../lib/utils/connect');
-// const app = require('../../lib/app');
-// const User = require('../../lib/models/User');
-// const request = require('supertest');
-// const mongoose = require('mongoose');
+require('dotenv').config();
+require('../../lib/utils/connect')();
+const connect = require('../../lib/utils/connect');
+require('dotenv').config();
+const connect = require('../../lib/utils/connect');
+const app = require('../../lib/app');
+const User = require('../../lib/models/User');
+const request = require('supertest');
+const mongoose = require('mongoose');
 
-// describe('User Model', () => {
-//   const createUser = (username) => {
-//     return User.create({
-//       username, 
-//       password: 'password'
-//     })
-//       .then(res =>  res.body);
-//   };
-//   beforeAll(() => {
-//     connect();
-//   });
-//   beforeEach(done => {
-//     mongoose.connection.dropDatabase(done);
-//   });
-//   afterAll(done => {
-//     mongoose.connection.close(done);
-//   });
-// });
+describe('User Model', () => {
+  const createUser = (username) => {
+    return User.create({
+      username, 
+      password: 'password',
+      role: 'owner'
+    })
+      .then(res =>  res.body);
+  };
+
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(done => {
+    mongoose.connection.dropDatabase(done);
+  });
+  afterAll(done => {
+    mongoose.connection.close(done);
+  });
+
+  it('allows a user to sign up', () => {
+    return request(app) 
+      .post('/auth/signup')
+      .send({ 
+        username: 'test2', 
+        password: 'password', 
+        role: 'owner' 
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          user: {
+            _id: expect.any(String),
+            username: 'test2',
+            role: 'owner'
+          },
+          token: expect.any(String)
+        });
+      });
+  });
+
+  it('allows user to sign in', () => {
+    return createUser('meeee1')
+      .then(() => {
+        return request(app)
+          .post('/auth/signin')
+          .send({ username: 'meeee1', password: 'password' });
+      })
+      .then(res => {
+        expect(res.body).toEqual({ 
+          user: {
+            _id: expect.any(String),
+            username: 'meeee1',
+            role: 'owner'
+          },
+          token: expect.any(String)
+        });
+      });
+  });
+  
+  it('has verify route', () => {
+    return createUser('weeee1')
+      .then(() => {
+        return request(app)
+          .post('/auth/signin')
+          .send({ username: 'weeee1', password: 'password' })
+          .then(res => res.body.token);
+      })
+      .then(token => {
+        return request(app)
+          .get('/auth/verify')
+          .set('Authorization', `Bearer ${token}`);                    
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          username: 'weeee1',
+          _id: expect.any(String),
+          role: 'owner'
+        });
+      }); 
+  });
+});
