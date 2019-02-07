c3a92d844ef42c7d67aa4b08efa61107e229d017
Shaba9 <shaba92@gmail.com>
2019-02-06 17:22:34 -0800

    get suggested zoos by zip and radius passes

--- test/routes/zoo-search.test.js
+++ test/routes/zoo-search.test.js
@@ -11,4 +11,26 @@ describe('zoo model', () => {
         rating: 4.5
       }]));
   });
+
+  it('gets suggested zoos by zip and radius', () => {
+    return request(app)
+      .get('/zoos/search/findZoo?zip=97221&radius=10000')
+      .then(res => expect(res.body).toEqual([
+        {
+          name: 'Oregon Zoo',
+          address: '4001 Southwest Canyon Road, Portland, OR 97221',
+          rating: 4.5
+        },
+        {
+          name: 'Nature Exploration Station',
+          address: '4027 Southwest Canyon Road, Portland, OR 97221',
+          rating: 5
+        },
+        {
+          name: 'Alpacas of Oregon',
+          address: '21345 SW Aebischer Rd, Sherwood, OR 97140',
+          rating: 4.7
+        }
+      ]));
+  });
 });
