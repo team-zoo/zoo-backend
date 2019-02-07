eb419cb83024d0dac30ed25b0d1c13694b48a640
Kristin Hortsch <kristinhortsch@gmail.com>
2019-02-06 17:43:30 -0800

    able to query for a certain animal in a mythical animal's animalCombination array

--- test/routes/mythicalAnimals.test.js
+++ test/routes/mythicalAnimals.test.js
@@ -28,12 +28,29 @@ describe('mythical animal model', () => {
       });
   });
 
-  it('gets a specific mythical animal from search query', () => {
+  it('gets a specific mythical animal from animal search query', () => {
     return request(app)
       .get('/mythicalAnimals/search/name/q?name=Sky%20Bison')
       .then(res => {
-        console.log(res.body);
         expect(res.body[0].name).toEqual('Sky Bison');
+      });
+  });
+
+  it('gets list of qualifying mythical animals from habitat search query', () => {
+    return request(app)
+      .get('/mythicalAnimals/search/habitat/q?habitat=Fire%20Nation')
+      .then(res => {
+        expect(res.body[0].habitat).toEqual('Fire Nation');
+        expect(res.body).toHaveLength(3);
+      });
+  });
+
+  it('gets a list of qualifying animals from animalCombination search query', () => {
+    return request(app)
+      .get('/mythicalAnimals/search/animalCombination/q?animalCombination=Koala')
+      .then(res => {
+        console.log(res.body, 'RES');
+        expect(res.body[0].animalCombination).toContain('Koala');
       });
   });
 });
